596ab0b10d634876a214e5ab99fa53fe425bcd6f
Janiaut <thomas.janiaut@epitech.eu>
2022-12-08 14:25:53 +0100

    FIX_VGA_MAKE_COLOR_rework-function-with-bitshift-

--- src/utils/VGA/VGA_color_macros.h
+++ src/utils/VGA/VGA_color_macros.h
@@ -1,34 +1,33 @@
 #ifndef TACOS_COLOR_MACROS_H
     #define TACOS_COLOR_MACROS_H
 
-#define VGA_BLACK 0
-#define VGA_BLUE 1
-#define VGA_GREEN 2
-#define VGA_CYAN 3
-#define VGA_RED 4
-#define VGA_PURPLE 5
-#define VGA_BROWN 6
-#define VGA_GRAY 7
-#define VGA_DARK_GRAY 8
-#define VGA_LIGHT_BLUE 9
-#define VGA_LIGHT_GREEN A
-#define VGA_LIGHT_CYAN B
-#define VGA_LIGHT_RED C
-#define VGA_LIGHT_PURPLE D
-#define VGA_YELLOW E
-#define VGA_white F
+    #define VGA_BLACK 0x0
+    #define VGA_BLUE 0x1
+    #define VGA_GREEN 0x2
+    #define VGA_CYAN 0x3
+    #define VGA_RED 0x4
+    #define VGA_PURPLE 0x5
+    #define VGA_BROWN 0x6
+    #define VGA_GRAY 0x7
+    #define VGA_DARK_GRAY 0x8
+    #define VGA_LIGHT_BLUE 0x9
+    #define VGA_LIGHT_GREEN 0xA
+    #define VGA_LIGHT_CYAN 0xB
+    #define VGA_LIGHT_RED 0xC
+    #define VGA_LIGHT_PURPLE 0xD
+    #define VGA_YELLOW 0xE
+    #define VGA_WHITE 0xF
 
 /**
- * @brief Create the hexa value to set the background and frontground to the string
+ * @brief Create the address to set the background and frontground to the string
  * @param BG: Background color of the string
  * @param FG frontground color of the string
- * @fonction C_CONC concatenate the value passed in parameters
+ * @param ADR address of the color
+ * @fonction APPLY_MASK apply mask on the address its allows to set the 4 hard byte to 0
  */
 
-#define C_CONC(BG, FD) C_CONC_(BG, FD)
-#define C_CONC_(BG, FD) BG##FR
-
-#define VGA_MAKE_COLOR(BG, FD) C_CONC(0x, C_CONC(BG, FD))
-
+    #define APPLY_MASK(ADR) __APPLY_MASK__(ADR)
+    #define __APPLY_MASK__(ADR) ADR & 0b00001111
+    #define VGA_MAKE_COLOR(BG, FG) (((__APPLY_MASK__(BG)) << 4) | (__APPLY_MASK__(FG)))
 
 #endif //TACOS_COLOR_MACROS_H
