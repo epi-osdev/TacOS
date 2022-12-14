13dd83db75ccc1e841aab7b9b5a12d443da71df8
messtt <corentin.albertus@epitech.eu>
2022-12-14 15:37:13 +0100

    correction s1[1] to s1[i]

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -4,9 +4,9 @@
 size_t strcmp(char const *s1, char const *s2)
 {
     for (int i = 0; s1[i] != '\0'; i++) {
-        if (s1[1] > s2[i])
+        if (s1[i] > s2[i])
             return 1;
-        if (s1[1] < s2[i])
+        if (s1[i] < s2[i])
             return -1;
     }
     return 0;
