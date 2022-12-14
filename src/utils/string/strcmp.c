5c6894acbc32f16b0deb52b03d486a3cdd314d3a
messtt <corentin.albertus@epitech.eu>
2022-12-14 16:56:02 +0100

    CORENTIN_ALBERTUS_FIX_STRCMP

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -3,11 +3,15 @@
 
 size_t strcmp(char const *s1, char const *s2)
 {
-    for (int i = 0; s1[i] != '\0'; i++) {
-        if (s1[i] > s2[i])
+    if (s1 == NULL || s2 == NULL)
+        return 84;
+    int i = 0;
+    do {
+        if (s1[i] > s2[i] || s2[i] == '\0' && s1[i] != '\0')
             return 1;
-        if (s1[i] < s2[i])
+        if (s1[i] < s2[i] || s1[i] == '\0' && s2[i] != '\0')
             return -1;
-    }
+        i++;
+    } while (s1[i] != '\0' || s2[i] != '\0');
     return 0;
 }
