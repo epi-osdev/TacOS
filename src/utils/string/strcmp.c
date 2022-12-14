0eb39e0da7a65cda52f4a0085340d15fc0ef5571
messtt <corentin.albertus@epitech.eu>
2022-12-14 14:12:54 +0100

    fix header & Use const Use '\0' to stop change int to size_t

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -1,10 +1,9 @@
 #include "strlen.h"
 #include "strcmp.h"
 
-int strcmp(char *s1, char *s2)
+size_t strcmp(char const *s1, char const *s2)
 {
-    int len_s1 = strlen(s1);
-    for (int i = 0; i < len_s1; i++) {
+    for (int i = 0; len_s1[i] != '\0'; i++) {
         if (s1[1] > s2[i])
             return 1;
         if (s1[1] < s2[i])
--- src/utils/string/strcmp.h
+++ src/utils/string/strcmp.h
@@ -1,13 +1,15 @@
 #ifndef __EOS_UTILS_STRING_STRCMP_H__
     #define __EOS_UTILS_STRING_STRCMP_H__
 
-    #include "types.h"
+#include "types.h"
 
     /**
     * @brief: a function compares the two strings
     * @param s1: the string that will be compared
     * @param s2: the string that compares
-    * @return: 0 if the s1 and s2 are equal | -1 negative value if s1 is less than s2 | 1 positive value if s1 is greater than s2.
+    * @return -1: negative value if s1 is less than s2
+    * @return 0: the s1 and s2 are equal
+    * @return 1: positive value if s1 is greater than s2.
     */
 
 int strcmp(char * s1, char * s2);
