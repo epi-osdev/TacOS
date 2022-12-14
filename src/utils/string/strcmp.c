5a0d9fc7d0def17c98bbbcc86f51b3c29f007ed6
messtt <corentin.albertus@epitech.eu>
2022-12-14 15:10:53 +0100

    correction unknow variable

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -3,7 +3,7 @@
 
 size_t strcmp(char const *s1, char const *s2)
 {
-    for (int i = 0; len_s1[i] != '\0'; i++) {
+    for (int i = 0; s1[i] != '\0'; i++) {
         if (s1[1] > s2[i])
             return 1;
         if (s1[1] < s2[i])
