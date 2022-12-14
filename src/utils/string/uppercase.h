bcb743e625ead9788515f26c39f66ff37fb60d37
Allan Leherpeux <allan.leherpeux@epitech.eu>
2022-12-14 17:45:52 +0100

    [ADD]_uppercase-header

---
+++ src/utils/string/uppercase.h
@@ -0,0 +1,11 @@
+#ifndef __EOS_UTILS_STRING_UPPERCASE_H__
+    #define __EOS_UTILS_STRING_UPPERCASE_H__
+
+/**
+* @brief This function changes all the characters of a 
+* string to uppercase if they are not already uppercase
+* @param str the string you want to capitalize
+*/
+void uppercase(char *str);
+
+#endif
