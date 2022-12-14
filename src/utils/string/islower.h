5809156bf3791daea8d184cc68d2ba4b51ede32e
Allan Leherpeux <allan.leherpeux@epitech.eu>
2022-12-14 17:43:29 +0100

    [ADD]_islower-header-for-islower.c

---
+++ src/utils/string/islower.h
@@ -0,0 +1,13 @@
+#ifndef __EOS_UTILS_STRING_ISLOWER_H__
+    #define __EOS_UTILS_STRING_ISLOWER_H__
+
+/**
+* @brief This function returns 1 if the given character
+* is lowercase, otherwise it returns 0
+* @param c The character to check
+* @return: 1 if the given character
+* is lowercase, else 0
+*/
+int islower(char c);
+
+#endif
