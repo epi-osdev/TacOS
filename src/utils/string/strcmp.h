6fd8d6e311223a7ee3fb8950032d4dcb6922615e
messtt <corentin.albertus@epitech.eu>
2022-12-23 21:44:31 +0100

    change information of return

--- src/utils/string/strcmp.h
+++ src/utils/string/strcmp.h
@@ -7,9 +7,8 @@
 * @brief: a function compares the two strings
 * @param s1: the string that will be compared
 * @param s2: the string that compares
-* @return 2: If a character in s1 is less than the character at the same index in s2
 * @return 0: If s1 and s2 are equal
-* @return 1: If a character in s1 is greater than the character at the same index in s2
+* @return s1 - s2: If s1 and s2 are different
 */
 
 int strcmp(char * s1, char * s2);
