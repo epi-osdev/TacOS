8f82c3c1d111be2e7843268b903f62835a6e8a52
messtt <corentin.albertus@epitech.eu>
2022-12-23 18:18:51 +0100

    CORENTIN_ALBERTUS_STRCMP_FIX

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -1,17 +1,16 @@
-#include "strlen.h"
 #include "strcmp.h"
 
-size_t strcmp(char const *s1, char const *s2)
+size_t my_strcmp(char const * s1, char const * s2)
 {
-    if (s1 == NULL || s2 == NULL)
-        return 84;
-    int i = 0;
-    do {
-        if (s1[i] > s2[i] || s2[i] == '\0' && s1[i] != '\0')
+    for (int i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if (s1[i] < s2[i])
+            return 2;
+        if (s1[i] > s2[i])
             return 1;
-        if (s1[i] < s2[i] || s1[i] == '\0' && s2[i] != '\0')
-            return -1;
-        i++;
-    } while (s1[i] != '\0' || s2[i] != '\0');
-    return 0;
+    }
+    if (s1[i] == '\0' && s2[i] == '\0')
+        return 0;
+    if (s1[i] == '\0')
+        return 2;
+    return 1;
 }
--- src/utils/string/strcmp.h
+++ src/utils/string/strcmp.h
@@ -7,9 +7,9 @@
 * @brief: a function compares the two strings
 * @param s1: the string that will be compared
 * @param s2: the string that compares
-* @return -1: negative value if s1 is less than s2
-* @return 0: the s1 and s2 are equal
-* @return 1: positive value if s1 is greater than s2.
+* @return 2: If a character in s1 is less than the character at the same index in s2
+* @return 0: If s1 and s2 are equal
+* @return 1: If a character in s1 is greater than the character at the same index in s2
 */
 
 int strcmp(char * s1, char * s2);
