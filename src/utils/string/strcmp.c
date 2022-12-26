ca5183a1a437f16e512d61bf31cc642a19fa8d61
messtt <corentin.albertus@epitech.eu>
2022-12-26 08:21:40 +0100

    change explaination of return in strcmp.h and optimize strcmp.c fonction

--- src/utils/string/strcmp.c
+++ src/utils/string/strcmp.c
@@ -5,7 +5,7 @@ int strcmp(char const * s1, char const * s2)
     if (s1 == NULL || s2 == NULL)
         return (s1 == s2) ? 0 : ((s1 == NULL) ? -1 : 1);
     while (*s1 && *s2) {
-        if (*s1 > *s2 || *s1 < *s2)
+        if (*s1 != *s2)
             return *s1 - *s2;
         s1++;
         s2++;
