3e30844b0227d9ed4c0c285fa8ad673df45c2498
pulk66-s <hugo.marrasse@epitech.eu>
2023-01-18 20:30:27 +0100

    [ADD] Add of comments in the VESA

--- src/drivers/vesa/config.h
+++ src/drivers/vesa/config.h
@@ -1,7 +1,16 @@
 #ifndef __TOS_DRIVERS_GUI_CONFIG_H__
     #define __TOS_DRIVERS_GUI_CONFIG_H__
 
+/**
+ * @brief The width of the window in pixels
+ * It's used to get the VESA mode width
+*/
     #define WINDOW_WIDTH 800
+
+/**
+ * @brief The height of the window in pixels
+ * It's used to get the VESA mode height
+*/
     #define WINDOW_HEIGHT 600
 
 #endif
--- src/drivers/vesa/init.h
+++ src/drivers/vesa/init.h
@@ -1,6 +1,10 @@
 #ifndef __GUI_INIT_H__
     #define __GUI_INIT_H__
 
+/**
+ * @brief Initializes the GUI
+ * Check the VESA mode and set the window size
+*/
 void init_gui();
 
 #endif
